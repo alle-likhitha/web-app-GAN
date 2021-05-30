9a864690bcb0ae0d884e3e7647edcf113eaa873b
alle-likhitha <likhitha.alle@gmail.com>
2021-05-30 19:07:48 +0530

    Added few web pages

--- src/containers/Images/Images.js
+++ src/containers/Images/Images.js
@@ -1,29 +1,51 @@
 import React, {Component} from 'react';
 import classes from "./Images.module.css";
 import ia from '../../assets/images/birdimage.jpg';
-
+// import Ima from '../../assets/testimages';
 
 
 class Images extends Component{
-    render(){
 
-        console.log(this.props.data)
-    let contents = null
-    // if(this.props.data){
-    //     contents = this.props.data.map( idata =>{
-    //         console.log(idata)
-    //         return <div></div>
-    //     })
+    
+    render(){
+        function importAll(r) {
+            console.log(r)
+            let images = [];
+            // r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+            r.keys().map((item, index) => { 
+                console.log(r(item).default)
+                // images[index] = r(item).default; 
+                images.push(r(item).default);
+            });
+            return images;
+          }
+          
+          const images = importAll(require.context('../../assets/testimages', false, /\.(png|jpe?g|svg)$/));
+          
+        console.log(images)
+        // console.log(this.props.data)
+        let contents = null
+    if(images){
+        // contents = this.props.data.map( idata =>{
+        //     console.log(idata)
+        //     return <div><img className={classes.ImgTag} src={ia} alt="bird" /></div>
+        // })
+        contents = images.map( idata =>{
+            console.log(idata)
+            return <div><img className={classes.ImgTag} src={ia} alt="bird" key={idata} /></div>
+        })
         
-    // }
-    contents= <div className={classes.ImgDiv}>
-        <img className={classes.ImgTag} src={ia} alt="bird" />
-        <img className={classes.ImgTag} src={ia} alt="bird" />
-        <img className={classes.ImgTag} src={ia} alt="bird" />
-    </div>
+    }
+        // contents= <div className={classes.ImgDiv}>
+        //     <img className={classes.ImgTag} src={ia} alt="bird" />
+        //     <img className={classes.ImgTag} src={ia} alt="bird" />
+        //     <img className={classes.ImgTag} src={ia} alt="bird" />
+        //     </div>
         return(
             <div className={classes.Images}>
+                <div className={classes.ImgDiv}>
                 {contents}
+                </div>
             </div>
         );
     }
--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -3,10 +3,13 @@ import classes from "./MainPage.module.css";
 import Button from '../../ui/Button/Button';
 import {FaEarlybirds} from 'react-icons/fa';
 import Images from '../Images/Images';
+import axios from 'axios';
+
 class MainPage extends Component{
     state={
         text : "",
-        valu: false
+        valu: false,
+        err: false
     }
 
     onChangeHandler = ( event ) => {
@@ -16,13 +19,37 @@ class MainPage extends Component{
     }
     onSubmitHandler = () => {
         console.log(this.state.text)
-        this.setState({valu:true});
+        this.setState({valu:true, err:false});
+    }
+    onErrorHandler = ()=>{
+        console.log(this.state.err)
+        this.setState({err:true});
+    }
+
+    DataPostHandler = props =>{
+        axios.get("http://localhost:9000/main/post-text?text="+this.state.text)
+        .then(res =>{
+            console.log(res.data);
+            this.onSubmitHandler();
+        })
+        .catch(err =>{
+            console.log(err);
+            this.onErrorHandler();
+        })
+    }
+    onRefreshHandler = () => {
+        console.log(this.state.text)
+        this.setState({valu:false, text:""});
     }
 
     render(){
         let imagesa = null
+        let err = null;
         if(this.state.valu){
             imagesa = <Images />
+        }
+        if(this.state.err){
+            err = <h3 className={classes.err}>Text Box is empty! Please add some text.</h3>
         }
         return(
             <div className={classes.MainPage}>
@@ -33,11 +60,13 @@ class MainPage extends Component{
                 <textarea id="input" rows="6" color="light blue" cols="60" value={this.state.text} onChange={this.onChangeHandler} className={classes.textarea}></textarea>
                 <br></br>
                 <div className={classes.Seconddiv} >
-                <Button iconname={{icon: FaEarlybirds}} clicked={this.onSubmitHandler}>Submit</Button>
+                <Button iconname={{icon: FaEarlybirds}} clicked={this.DataPostHandler}>Submit</Button>
+                <Button iconname={{icon: FaEarlybirds}} clicked={this.onRefreshHandler}>Refresh</Button>
                 </div>
                 <br></br>
                 {this.state.text}
-                
+                <br></br>
+                {err}
                 {imagesa}
                 
             </div>
